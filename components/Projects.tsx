7c00be80002b49fb4a86c03c763cea6c2672347f
Dale B <daleban.dev@gmail.com>
2021-11-09 21:14:26 +0800

    Remove unused files

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,7 @@ interface ProjectsProps {}
 
 export const Projects: React.FC<ProjectsProps> = ({}) => {
   return (
-    <section className="bg-transparent min-h-[200vh] py-[300px] flex flex-col justify-between gap-96 items-center">
+    <section className="bg-transparent min-h-[200vh] py-[300px] flex flex-col justify-between gap-96 items-center px-20">
       {projects.map((img: StaticImageData, i: number) => (
         <div key={i} className="light-overlay">
           <Image src={img} alt={img.toString()} height={350} width={640} />
