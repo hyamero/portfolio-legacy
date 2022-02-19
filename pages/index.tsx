d2a1a624a40a6a863135d3c2c5135118d7ab0124
Dale <daleban.dev@gmail.com>
2022-02-19 18:00:10 +0800

    Remove vanilla smooth scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,63 +7,16 @@ import { About } from "../components/About";
 import { Projects } from "../components/Projects";
 import { Footer } from "../components/Footer";
 
-import useWindowSize from "../hooks/useWindowSize";
-
 const Home: NextPage = () => {
-  const size = useWindowSize();
-
-  const layout = useRef<HTMLDivElement>(null);
-  const scrollContainer = useRef<HTMLDivElement>(null);
-
-  const skewConfigs = {
-    ease: 0.1,
-    current: 0,
-    previous: 0,
-    rounded: 0,
-  };
-
-  useEffect(() => {
-    document.body.style.height = `${
-      scrollContainer.current?.getBoundingClientRect().height
-    }px`;
-  }, [size?.height]);
-
-  useEffect(() => {
-    requestAnimationFrame(() => {
-      skewScrolling();
-    });
-  }, []);
-
-  const skewScrolling = () => {
-    skewConfigs.current = window.scrollY;
-    skewConfigs.previous +=
-      (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
-    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
-
-    //variables
-    const difference = skewConfigs.current - skewConfigs.rounded;
-    const acceleration = difference / size!.width;
-    const velocity = +acceleration;
-    // const skew = velocity * 7.5;
-
-    try {
-      scrollContainer.current!.style.transform = `translate3d(0, -${skewConfigs.rounded}px, 0)`;
-    } catch (err) {
-      return;
-    }
-
-    requestAnimationFrame(() => skewScrolling());
-  };
-
   return (
-    <div ref={layout} className="fixed top-0 left-0 h-[100%] w-[100%]">
+    <div className="h-[100%] w-[100%]">
       <Head>
         <title>Dale Bañares</title>
         <meta name="description" content="Dale Bañares Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main ref={scrollContainer} className="pointer-events-none">
+      <main>
         <Heading />
         <About />
         <Projects />
