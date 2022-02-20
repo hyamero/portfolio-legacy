33a4e4c54b95c7c8250b8a0151e4c8164c962d8a
Dale <daleban.dev@gmail.com>
2022-02-20 17:56:41 +0800

    Update projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,7 @@ interface ProjectsProps {}
 
 export const Projects: React.FC<ProjectsProps> = ({}) => {
   return (
-    <div
+    <section
       data-scroll-section
       id="projects"
       className=" min-h-[200vh] py-[300px] flex flex-col justify-between gap-96 items-center px-10 relative"
@@ -29,20 +29,26 @@ export const Projects: React.FC<ProjectsProps> = ({}) => {
           data-scroll-direction="horizontal"
           data-scroll-speed={0.6 + i}
           key={i}
-          className="cursor-pointer z-[2] overflow-hidden"
+          className="cursor-pointer z-[2] overflow-hidden "
           href={project.link}
         >
-          <div data-scroll data-scroll-speed={1}>
+          <div data-scroll data-scroll-speed={1} className="relative">
             <Image
               priority
               src={project.img}
               alt={project.title}
               height={350}
               width={640}
+              quality={100}
+              placeholder="blur"
+              className="transition-transform ease-out duration-300 hover:scale-110 hover:opacity-80"
             />
           </div>
+          <p className="font-helvetica text-3xl uppercase absolute bottom-8 left-8 text-white mix-blend-difference whitespace-nowrap space-x-8">
+            <span>{project.title}</span>
+          </p>
         </a>
       ))}
-    </div>
+    </section>
   );
 };
