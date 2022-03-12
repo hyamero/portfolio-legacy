9d48e9ceb8adf2be2e739385df752c147f1aac80
Dale <daleban.dev@gmail.com>
2022-03-12 21:48:40 +0800

    Update meta image link

--- next-seo-config.js
+++ next-seo-config.js
@@ -2,7 +2,7 @@ const description =
   "Front-end developer based in the Philippines. A creative developer with a passion for building beautiful and interactive websites.";
 
 const imgUrl =
-  "https://raw.githubusercontent.com/hyamero/portfolio/main/public/img/Header.png";
+  "https://raw.githubusercontent.com/hyamero/portfolio/dev/public/img/Footer.png";
 
 export default {
   title: "Joseph Dale Bañares - Creative Developer",
