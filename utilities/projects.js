fadb404600f478affa0aea756c3d238cc60e2d8c
Dale <daleban.dev@gmail.com>
2022-03-14 11:38:53 +0800

    Update project image imports

--- utilities/projects.js
+++ utilities/projects.js
@@ -1,6 +1,6 @@
 import boisson from "../public/img/projects/boisson.jpg";
-import collectiveApis from "../public/img/projects/collective-apis.jpg";
-import cayenne from "../public/img/projects/CayennePage.jpg";
+import collectiveApis from "../public/img/projects/collective-api.jpg";
+import cayenne from "../public/img/projects/car.jpg";
 import haboleon from "../public/img/projects/haboleon.jpg";
 
 export const projects = [
