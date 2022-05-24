21ce15c3c8059d07bf31332f76c84e6407e5b4b4
Dale <daleban.dev@gmail.com>
2022-05-24 14:44:06 +0800

    Add iphone config in projects

--- utilities/projects.js
+++ utilities/projects.js
@@ -2,8 +2,14 @@ import boisson from "../public/img/projects/boisson.jpg";
 import collectiveApis from "../public/img/projects/collective-api.jpg";
 import cayenne from "../public/img/projects/car.jpg";
 import haboleon from "../public/img/projects/haboleon.jpg";
+import iphone from "../public/img/projects/iphone.jpg";
 
 export const projects = [
+  {
+    title: "iPhone Config",
+    img: iphone,
+    link: "https://iphone-config.vercel.app/",
+  },
   {
     title: "Collective Apis",
     img: collectiveApis,
