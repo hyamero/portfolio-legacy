043381f8842c7c3a5c500c2dd4f3fe40cb0df4dd
Dale <daleban.dev@gmail.com>
2022-05-26 08:19:14 +0800

    chore: fill learning experience content

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { projects } from "../utilities/projects";
-import { Footer } from "./Footer";
+import { academics } from "../utilities/academics";
 
 export const Experiences: React.FC = ({}) => {
   return (
@@ -10,38 +9,35 @@ export const Experiences: React.FC = ({}) => {
       id="projects"
       className=" relative z-[8] mt-[50px] flex min-h-[200vh] flex-col items-center justify-between gap-96 overflow-hidden bg-black px-10 pt-[150px]  pb-[300px] sm:gap-52 sm:py-[200px]"
     >
-      {projects.map((project: any, i: number) => (
-        <React.Fragment key={project.title}>
+      {academics.map((academic: any, i: number) => (
+        <React.Fragment key={academic.title}>
           <span
             data-scroll
             data-scroll-speed={7 + i}
             data-scroll-direction="horizontal"
             className="text-clamp pointer-events-none relative z-[3] select-none whitespace-nowrap font-playfair text-9xl italic text-white/25"
           >
-            {project.title} <span className="relative top-3">*</span>{" "}
-            {project.title} <span className="relative top-3">*</span>{" "}
-            {project.title}{" "}
+            {academic.title} <span className="relative top-3">*</span>{" "}
+            {academic.title} <span className="relative top-3">*</span>{" "}
+            {academic.title}{" "}
             <span className="relative top-3 animate-spin">*</span>{" "}
-            {project.title} <span className="relative top-3">*</span>{" "}
-            {project.title} <span className="relative top-3">*</span>{" "}
-            {project.title} <span className="relative top-3">*</span>{" "}
-            {project.title} <span className="relative top-3">*</span>
+            {academic.title} <span className="relative top-3">*</span>{" "}
+            {academic.title} <span className="relative top-3">*</span>{" "}
+            {academic.title} <span className="relative top-3">*</span>{" "}
+            {academic.title} <span className="relative top-3">*</span>
           </span>
-          <a
+          <div
             data-scroll
             data-scroll-direction="horizontal"
             data-scroll-speed={7 + i}
             key={i}
-            className="clip-0 img cursor-pointer overflow-hidden"
-            href={project.link}
-            target="_blank"
-            rel="noreferrer"
+            className="clip-0 img overflow-hidden"
           >
             <div data-scroll data-scroll-speed={2} className="relative">
               <Image
                 priority
-                src={project.img}
-                alt={project.title}
+                src={academic.img}
+                alt={academic.title}
                 height={405}
                 width={720}
                 quality={90}
@@ -53,10 +49,13 @@ export const Experiences: React.FC = ({}) => {
                 data-scroll-direction="horizontal"
                 className="text-clamp absolute bottom-5 left-20 z-[10] whitespace-nowrap font-playfair italic text-white mix-blend-difference"
               >
-                {project.title}
+                {academic.title}
               </span>
+              <p className="sub-text text-ylw-grad absolute -bottom-5 right-0 text-left">
+                {academic.desc}
+              </p>
             </div>
-          </a>
+          </div>
         </React.Fragment>
       ))}
     </section>
---
+++ utilities/academics.js
@@ -0,0 +1,27 @@
+import cs50 from "../public/img/academics/cs50.jpg";
+import youtube from "../public/img/academics/youtube.jpg";
+import autodidact from "../public/img/academics/autodidact.jpg";
+import discord from "../public/img/academics/discord.jpg";
+
+export const academics = [
+  {
+    title: "Harvard CS50",
+    img: cs50,
+    desc: "2021 - Taken Harvard's Introductory Computer Science Course",
+  },
+  {
+    title: "Education",
+    img: youtube,
+    desc: "2021 - Youtube became my main source of education",
+  },
+  {
+    title: "Autodidact",
+    img: autodidact,
+    desc: "2021 to present - Self-taught Web Developer",
+  },
+  {
+    title: "Client",
+    img: discord,
+    desc: "2022 - Portfolio Worked! Got my first Client!",
+  },
+];
